04bae6f3d535feb5787df71828ef2919300dc6ab
necrowolf132 <manjou132@gmail.com>
2024-10-05 14:42:13 -0400

    Agregando ssh a contenedor de produccion para poder loguear V2

--- set_ssh.sh
+++ set_ssh.sh
@@ -3,5 +3,7 @@ mkdir /var/run/sshd
 echo "root:25448132" | chpasswd
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 service ssh start
+systemctl restart ssh
 apache2-foreground
